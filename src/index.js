0c2d65bf9cc6ee43bd525f05903db83275e60233
Nancy Bechtold <nancyleebechtold@gmail.com>
2024-06-11 17:27:55 -0600

    v1 ramen challenge

--- src/index.js
+++ src/index.js
@@ -1,26 +1,85 @@
 // index.js
+fetch("http://localhost:3000/ramens")
+.then((r) =>r.json())
+.then((ramens)=>{
+  console.log(ramens)
+  ramens.forEach((ramen) => {
+    displayRamens(ramen)
+  })
 
-// Callbacks
-const handleClick = (ramen) => {
-  // Add code
-};
+})
 
-const addSubmitListener = () => {
-  // Add code
+function handleClick(ramen) {
+  const ramenName = document.querySelector(".name")
+  const ramenRestaurant = document.querySelector(".restaurant")
+  const ramenImage = document.querySelector(".detail-image")
+  const ramenRating = document.querySelector("#rating-display")
+  const ramenComment = document.querySelector("#comment-display")
+
+  ramenName.textContent = ramen.name
+  ramenRestaurant.textContent = ramen.restaurant
+  ramenImage.src = ramen.image
+  ramenRating.textContent = ramen.rating 
+  ramenComment.textContent= ramen.comment
+  
+  
+}
+// // REAPLACED with function --const handleClick = (ramen) => {
+// //   // Add code
+// // };
+const addSubmitListener = document.querySelector("#new-ramen")
+addSubmitListener.addEventListener("submit", (e)=> {
+  e.preventDefault()
+  const newRamen = {
+    name: e.target["new-name"].value,
+    restaurant: e.target["new-restaurant"].value,
+    image: e.target["new-image"].value,
+    rating: e.target["new-rating"].value,
+    comment: e.target["new-comment"].value,
+
+  }
+  e.target["new-name"].value = ""
+  e.target["new-restaurant"].value = ""
+  e.target["new-image"].value = ""
+  e.target["new-rating"].value = ""
+  e.target["new-comment"].value = ""
+
+  displayRamens(newRamen)
+})
+
+
+
+function displayRamens(ramen) {
+  const disRamen = document.querySelector("#ramen-menu")
+  const image = document.createElement("img")
+  image.src = ramen.image
+
+  disRamen.append(image)
+
+  image.addEventListener('click', () => {
+    handleClick(ramen)
+    
+
+  })
 }
 
-const displayRamens = () => {
-  // Add code
-};
+// // REAPLACED with function --const displayRamens = () => {
+// //   // Add code
+// // };
 
-const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
+function main(ramen) {
+  displayRamens(ramen)
+  addSubmitListener(newRamen)
 }
 
+
+// // REAPLACED with function -- const main = () => {
+// //   // Invoke displayRamens here
+// //   // Invoke addSubmitListener here
+// // }
 main()
 
-// Export functions for testing
+// // Export functions for testing
 export {
   displayRamens,
   addSubmitListener,
